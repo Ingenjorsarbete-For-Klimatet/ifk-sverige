9f819ca1ed2c0f30a8e0d65c9e4b7f67b3065abb
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2024-01-02 14:20:16 +0100

    Return dataframe entry for index

--- src/text_search_server/search.py
+++ src/text_search_server/search.py
@@ -4,9 +4,10 @@ from fastapi import FastAPI
 from text_search_server import trie
 import geopandas as gpd
 
-text = gpd.read_file("/mnt/data/text_sverige.gpkg")
+text_pd = gpd.read_file("/mnt/data/text_sverige.gpkg")
 text = [
-    (text, index) for text, index in zip(text.textstrang.tolist(), text.index.tolist())
+    (text, index)
+    for text, index in zip(text_pd.textstrang.tolist(), text_pd.index.tolist())
 ]
 
 tdb = trie.TrieDB()
@@ -28,4 +29,5 @@ def search(input_text: str) -> dict:
     """
     tquery = trie.TrieQuery(tdb.root)
     all_words = tquery.search(input_text)
-    return {"input_text": input_text, "top_50_matches": all_words[:50]}
+    top_50_matches = [text_pd.iloc[y] for x, y in all_words[:50]]
+    return {"input_text": input_text, "top_50_matches": top_50_matches}
