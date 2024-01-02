7115f5a421a8c918f2950cd0db4c65e73600bfad
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2024-01-02 19:35:32 +0100

    Update search file

--- src/text_search_server/search.py
+++ src/text_search_server/search.py
@@ -1,25 +1,52 @@
 """Search module."""
 
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from text_search_server import trie
 import geopandas as gpd
 
-text_pd = gpd.read_file("/mnt/data/text_sverige.gpkg")
-text_pd = text_pd.to_crs("EPSG:4326")
-text_pd["geometry_xy"] = [
-    (x.coords.xy[0][0], x.coords.xy[1][0]) for x in text_pd.geometry
-]
-text_pd = text_pd.drop("geometry", axis=1)
-text = [
-    (text, index)
-    for text, index in zip(text_pd.textstrang.tolist(), text_pd.index.tolist())
-]
+DATABASE = {}
+FILE = "/mnt/data/text_sverige.gpkg"
 
-tdb = trie.TrieDB()
-for word in text:
-    tdb.insert(word)
 
-app = FastAPI()
+def initialise_data(file: str) -> tuple[gpd.GeoDataFrame, trie.TrieDB]:
+    """Initialise data.
+
+    Args:
+        file: file to load
+
+    Returns
+        tuple of geopandas object and trie database
+    """
+    text_pd = gpd.read_file(file)
+    text_pd = text_pd.to_crs("EPSG:4326")
+    text_pd["geometry_xy"] = [
+        (x.coords.xy[0][0], x.coords.xy[1][0]) for x in text_pd.geometry
+    ]
+    text_pd = text_pd.drop("geometry", axis=1)
+
+    text = [
+        (text, index)
+        for text, index in zip(text_pd.textstrang.tolist(), text_pd.index.tolist())
+    ]
+
+    tdb = trie.TrieDB()
+    for word in text:
+        tdb.insert(word)
+
+    return text_pd, tdb
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Load data on app startup."""
+    text_pd, tdb = initialise_data(FILE)
+    DATABASE["text_pd"] = text_pd
+    DATABASE["tdb"] = tdb
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/search/{input_text}")
@@ -32,6 +59,8 @@ def search(input_text: str) -> dict:
     Returns:
         top 50 found results for query
     """
+    text_pd = DATABASE["text_pd"]
+    tdb = DATABASE["tdb"]
     tquery = trie.TrieQuery(tdb.root)
     all_words = tquery.search(input_text)
     top_50_matches = [text_pd.iloc[y].to_dict() for x, y in all_words[:50]]
