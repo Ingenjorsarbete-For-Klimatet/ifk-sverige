f34c2eef8805cd41e3675a48be644641ccc3f2f5
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2024-01-29 22:42:53 +0100

    Remove configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +10,5 @@ export default defineConfig({
     },
   },
 
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react()],
 });
